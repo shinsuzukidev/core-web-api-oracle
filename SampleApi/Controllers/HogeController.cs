96dba069c10cbeb4dcfa0224a16166584c5d0d08
shin.suzuki <shin.suzuki.dev@gmail.com>
2025-01-25 00:53:59 +0900

    sample api add.

---
+++ SampleApi/Controllers/HogeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HogeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("Hoge Index");
+        }
+    }
+}
